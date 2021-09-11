f9f975c7349bae157140cd5bb1e538dfd1066984
KevinDai11 <71230236+KevinDai11@users.noreply.github.com>
2021-09-11 13:05:01 -0500

    Add files via upload
    image puller

---
+++ ImagePull.py
@@ -0,0 +1,49 @@
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
+from tkinter.ttk import *
+from PIL import Image
+import sys
+
+
+master = tk.Tk()
+
+master.geometry("400x400")
+
+
+
+def findImage():
+    root = tk.Tk()
+    root.withdraw()
+    files = filedialog.askopenfilenames()
+    try: 
+        im=Image.open(files)
+    except IOError:
+        messagebox.showerror("not an image")
+    
+        
+
+labelTextInstructions = Label(master,text = "this is main window \n select an image from file explorer").pack(pady =10)
+
+imagepullerButton = Button(master, text = "open file explorer" , command = findImage).pack(pady = 10)
+
+def on_closing():
+    if messagebox.askokcancel("Quit", "Do you want to quit?"):
+        master.destroy()
+
+master.protocol("WM_DELETE_WINDOW", on_closing)
+
+master.mainloop()
+
+sys.exit()
+
+
+
+
+
+
+
+
+
+
+    
