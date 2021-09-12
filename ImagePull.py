e1c6bb9828ad90dbedc1a73b4ee00fbed496c1aa
KevinDai11 <71230236+KevinDai11@users.noreply.github.com>
2021-09-11 22:49:12 -0500

    Update ImagePull.py
    compare image using MSE and SSIM .... might change to SIFT method.

--- ImagePull.py
+++ ImagePull.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import *
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 from tkinter.ttk import *
 from PIL import ImageTk, Image  
 import sys
@@ -14,16 +14,37 @@ master = tk.Tk()
 master.geometry("400x400")
 
 def findMSE(ImageA, ImageB):
-    mse = np.sum(ImageA.astype("float") - ImageB.astype("float")) ** 2
+    mse = np.sum((ImageA.astype("float") - ImageB.astype("float")) ** 2)
     mse /= float(ImageA.shape[0] * ImageA.shape[1])
     
     return mse;
 
-
 #image recoginition
-def start(ImageB):
-    print("asd")
-    #print(findMSE())
+def start(ImageStr):
+      
+    tempWin = Toplevel(master)
+    tempWin.geometry("400x400")
+    imgTemp = Image.open("C:/Users/Kevin Dai/Downloads/CLB2.jpeg")
+    imgTemp = imgTemp.resize((300,300),Image.ANTIALIAS)
+    imgTempP = ImageTk.PhotoImage(imgTemp)
+    labelTemp = tk.Label(tempWin,image = imgTempP)
+    labelTemp.image = imgTempP
+    labelTemp.place(x=50,y=0)
+    
+    ImageA = cv2.imread("C:/Users/Kevin Dai/Downloads/CLB2.jpeg")
+    ImageB = cv2.imread(ImageStr)
+    ImageB = cv2.cvtColor(ImageB, cv2.COLOR_BGR2GRAY)
+    ImageA = cv2.cvtColor(ImageA, cv2.COLOR_BGR2GRAY)
+    ImageA = cv2.resize(ImageA, (480,480),interpolation = cv2.INTER_AREA)
+    ImageB = cv2.resize(ImageB, (480,480),interpolation = cv2.INTER_AREA)
+    
+    imagea = Image.fromarray(ImageA)
+    imagea = ImageTk.PhotoImage(imagea)
+    labelTemp = tk.Label(tempWin, image = imagea)
+    labelTemp.image = imageas
+    
+    print(findMSE(ImageA, ImageB))
+    print(ssim(ImageA, ImageB))
 
 #choosing image from file explorer
 
@@ -36,11 +57,12 @@ def findImage():
         img=Image.open(files[0])
         img = img.resize((300, 300), Image.ANTIALIAS) 
         imgPrint =ImageTk.PhotoImage(img)
-        label1 = Label(image = imgPrint)
+        label1 = tk.Label(image = imgPrint)
         label1.image = imgPrint
         label1.place(x=50,y=0)
-        ButtonDecline = Button(master, text = "wrong image", command = findImage).pack(side = BOTTOM)
-        ButtonConfirm = Button(master, text = "correct image", command = start(imgPrint)).pack(side = BOTTOM)
+        
+        ButtonDecline = tk.Button(master, text = "wrong image", command = findImage).pack(side = BOTTOM)
+        ButtonConfirm = tk.Button(master, text = "correct image", command = lambda: start(files[0])).pack(side = BOTTOM)
         
     #catch error if file selected is not an image
     except IOError:
@@ -48,9 +70,9 @@ def findImage():
     
         
 #instruction text
-labelTextInstructions = Label(master,text = " select an image from file explorer").pack(pady =10)
+labelTextInstructions = tk.Label(master,text = " select an image from file explorer").pack(pady =10)
 #button to show file explorer
-imagepullerButton = Button(master, text = "open file explorer" , command = findImage).pack(pady = 10)
+imagepullerButton = tk.Button(master, text = "open file explorer" , command = findImage).pack(pady = 10)
 
 #exit
 def on_closing():
@@ -59,8 +81,6 @@ def on_closing():
 master.protocol("WM_DELETE_WINDOW", on_closing)
 
 master.mainloop()
-
-
 
 sys.exit()
 
@@ -72,8 +92,6 @@ sys.exit()
 
 
 
-
-    
 
 
     
