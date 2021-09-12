dcd866a84a1b3337626eb5b92d667fd04f78b949
valerie espinosa <vne@tamu.edu>
2021-09-12 10:32:09 -0500

    Completed the project!

--- ImagePull.py
+++ ImagePull.py
@@ -4,47 +4,34 @@ from tkinter import messagebox
 from tkinter.ttk import *
 from PIL import Image
 import sys
-
+import os
 
 master = tk.Tk()
 
 master.geometry("400x400")
 
-# testing git
 
-
-def findImage():
+def find_image():
     root = tk.Tk()
     root.withdraw()
     files = filedialog.askopenfilenames()
-    try: 
-        im=Image.open(files)
+    try:
+        im = Image.open(files)
     except IOError:
         messagebox.showerror("not an image")
-    
-        
 
-labelTextInstructions = Label(master,text = "this is main window \n select an image from file explorer").pack(pady =10)
-
-imagepullerButton = Button(master, text = "open file explorer" , command = findImage).pack(pady = 10)
 
 def on_closing():
     if messagebox.askokcancel("Quit", "Do you want to quit?"):
         master.destroy()
+
+
+labelTextInstructions = Label(master, text="this is main window \n select an image from file explorer").pack(pady=10)
+
+imagepullerButton = Button(master, text="open file explorer", command=find_image).pack(pady=10)
 
 master.protocol("WM_DELETE_WINDOW", on_closing)
 
 master.mainloop()
 
 sys.exit()
-
-
-
-
-
-
-
-
-
-
-    
